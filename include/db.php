ecd31aae308764dabc75dd906c73f841b889c67a
jagsher2002 <157498483+jagsher2002@users.noreply.github.com>
2024-04-16 11:07:50 -0400

    Updated db.php
    Extremely sorry, my credentials was wrong I put "L" instead of "I".

--- include/db.php
+++ include/db.php
@@ -1,3 +1,3 @@
 <?php
-$db = new PDO('mysql:host=172.31.22.43;dbname=Jagsher200567017', 'a_RwALIaGd', '');
+$db = new PDO('mysql:host=172.31.22.43;dbname=Jagsher200567017', 'Jagsher200567017', 'a_RwALIaGd');
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
